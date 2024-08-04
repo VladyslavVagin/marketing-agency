61dba9d3924eb4da287d4b05624069b10086d11d
VladyslavVagin <vladtwince@gmail.com>
2024-08-04 11:00:40 +0200

    update

---
+++ src/components/MainPage/Skills/ListSkills/ListSkills.tsx
@@ -0,0 +1,12 @@
+const ListSkills = () => {
+
+  return (
+    <ul className="flex flex-col items-center mt-14">
+      <li className="w-[270px] h-[272px] rounded-2xl bg-white shadow-xl flex items-center justify-center">
+        <div className="w-40 h-40"></div>
+      </li>
+    </ul>
+  );
+};
+
+export default ListSkills;
--- src/components/MainPage/Skills/Skills.tsx
+++ src/components/MainPage/Skills/Skills.tsx
@@ -1,9 +1,11 @@
 import SkillsTitle from "./SkillsTitle/SkillsTitle";
+import ListSkills from "./ListSkills/ListSkills";
 
 const Skills = () => {
   return (
     <div className="bg-[#f2f2f2] rounded-[50px] px-[30px] pt-[74px] pb-16">
       <SkillsTitle />
+      <ListSkills />
     </div>
   );
 };
