c3aff21d50cc417fc61bdd67d11905b10dd9aef2
VladyslavVagin <vladtwince@gmail.com>
2024-08-06 22:00:13 +0200

    update

--- src/components/ProjectsPage/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectsPage/ProjectCard/ProjectCard.tsx
@@ -1,8 +1,34 @@
 import { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { paytoneOne } from "@/app/layout";
 import { Project } from "@/types/project";
 
 const ProjectCard: FC<Project> = ({ project }) => {
-  return <div>ProjectCard</div>;
+  return (
+    <li className="w-[300px] h-[300px] bg-yellowIcon shadow-xl p-2 rounded-lg relative overflow-hidden border-4 border-accent flex flex-col items-center justify-center">
+      <Image
+        src={project.thumbnail}
+        alt={project.title}
+        layout="fill"
+        objectFit="cover"
+        className="filter brightness-50"
+      />
+      <span className="absolute top-2 right-2 p-1 bg-slate-700 text-white rounded-lg">
+        {project.category}
+      </span>
+      <h2 className={`${paytoneOne.className} title text-white relative z-20`}>
+        {project.title}
+      </h2>
+      <Link
+        href={`/portfolio/${project.path}`}
+        aria-label={`Go to details of project ${project.title}`}
+        className="relative z-30 mt-8 bg-accent px-3 py-1 rounded-xl text-white text-xl font-semibold"
+      >
+        See Details
+      </Link>
+    </li>
+  );
 };
 
 export default ProjectCard;
