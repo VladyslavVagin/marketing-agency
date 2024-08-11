25f49fb339267a4ec4f69a3d515f32cb0714a160
VladyslavVagin <vladtwince@gmail.com>
2024-08-11 19:56:20 +0200

    update

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -58,7 +58,7 @@ export default async function RootLayout({
       <NextIntlClientProvider messages={messages}>
         <body className={`${montserrat.className}`}>
           <Header />
-          <main className="bg-[#d1ebf5] min-h-screen">{children}</main>
+          <main className="bg-[#d1ebf5] min-h-screen overflow-hidden">{children}</main>
           <Footer />
           <Toast />
         </body>
